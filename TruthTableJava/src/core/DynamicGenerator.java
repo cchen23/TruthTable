30baeee5501e0a95a4e838f70a5a7bc872ed981b
Cathy Chen <cathy.chen@princeton.edu>
2015-11-28 16:52:11 -0500

    Error with printing output functions: names in text file don't necessarily (most probably don't) match variable names used in program. Maybe use a hash table to fix this?

--- TruthTableJava/src/core/DynamicGenerator.java
+++ TruthTableJava/src/core/DynamicGenerator.java
@@ -1,7 +1,18 @@
+/*****************************************************************************/
+/* DynamicGenerator.java                                                     */
+/*																			 */
+/* Purpose: Given a text file of boolean equations, prints a program that    */
+/* 		    will produce a csv format truth table based on the boolean       */
+/*          equations.                                                       */
+/*                                                                           */
+/* Compile: javac DynamicGenerator.java                                      */
+/* Run: java DynamicGenerator                                                */
+/* Author: Cathy Chen                                                        */
+/*****************************************************************************/
+
+
 package core;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,61 +22,113 @@ public class DynamicGenerator {
 		int numOutputs;
 		int numInputs;
 		String equation;
-		String[] eq;
-		int length;
 		
+		System.out.println("public class TruthTable {");
 		// Read equations from input file
-			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-			
-			// Get number of inputs and outputs
-			numOutputs = Integer.getInteger(reader.readLine());
-			numInputs = Integer.getInteger(reader.readLine());
+		//BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+		BufferedReader reader = new BufferedReader(new FileReader("src/test.txt"));
+		// Get number of inputs and outputs
+		numOutputs = Integer.parseInt(reader.readLine());
+		numInputs = Integer.parseInt(reader.readLine());
 			
 			// For each line, print method that calculates output
 			for (int i = 0; i < numOutputs; i++) {
 				equation = reader.readLine();
-				eq = equation.split(" ");
-				length = eq.length;
-				System.out.println("private int output" + i + "{");
-				System.out.print("int output = ");
-				for (int j = 0; j < length; j++) {
-					if (j % 2 == 0) {
-						int strLen = eq[j].length();
-						if (eq[j].charAt(strLen) == '\'') {
-							System.out.print("!" + eq[j].substring(0, strLen));
-						}
-						else System.out.print(eq[j]);
-					}
-					else {
-						System.out.print(eq[j]);
-					}
-				}
-				System.out.println(";");
-				
-				System.out.println("}");
+				printEquation(equation, i, numInputs);
 			}
 		
 		// Print loop that generates truth table and calls methods
+			indent(1);
 			System.out.println("public static void main(String[] args) {");
-			for (int i = 0; i < numInputs; i++) {
-				System.out.println("for int " + i + "= 0; " + i + " < 2; "
-						+ i + "++) {");
-			}
+			indent(2);;
+			printLoopHeader(numInputs);
 			
 			for (int i = 0; i < numOutputs; i++) {
-				System.out.print("System.out.print(out" + i + "(");
-				for (int j = 0; j < numInputs - 1; j++) {
-					System.out.print("in" + j + ",");
-				}
-				System.out.print("in" + numInputs + ");");
+				indent(3);
+				printOutput(numInputs, i);
 				if (i != numOutputs - 1) {
-					System.out.print(",");
+					System.out.print("System.out.print\",\"");
 				}
-				System.out.println();
+				else {
+					indent(3);
+					System.out.println("System.out.println();");
+				}
 			}
 			
 			for (int i = 0; i < numInputs; i++) {
+				indent(numInputs - i + 1);
 				System.out.println("}");
 			}
+			
+			indent(1);
+			System.out.println("}");
+			
+			reader.close();
+			System.out.println("}");
+	}
+	
+	// Prints private methods that will generate values of truth table outputs
+	private static void printEquation(String equation, int eqNum, int numInputs) {
+		String[] eq = equation.split(" ");
+		int length = eq.length;
+		indent(1);
+		System.out.print("private int output" + eqNum + "(");
+		for (int i = 0; i < numInputs; i++) {
+			System.out.print("in" + i);
+			if (i != numInputs - 1) {
+				System.out.print(", ");
+			}
+			else {
+				System.out.println(") {");
+			}
+		}
+		
+		indent(2);
+		System.out.print("int output = ");
+		for (int j = 2; j < length; j++) {
+			if (j % 2 == 0) {
+				int strLen = eq[j].length();
+				if (eq[j].charAt(strLen - 1) == '\'') {
+					System.out.print("!" + eq[j].substring(0, strLen));
+				}
+				else System.out.print(eq[j]);
+			}
+			else {
+				System.out.print(eq[j]);
+			}
+		}
+		System.out.println(";");
+		
+		indent(2);
+		System.out.println("if (output == 0) return 0;");
+		indent(2);
+		System.out.println("else return 1;");
+		indent(1);
+		System.out.println("}");
+	}
+	
+	// Prints the loop that will run through all possible input combinations
+	private static void printLoopHeader(int numInputs) {
+		for (int i = 0; i < numInputs; i++) {
+			System.out.println("for (int in" + i + " = 0; in" + i + " < 2; in"
+					+ i + "++) {");
+		}
+	}
+	
+	// Prints print statement that will print outputs in csv format
+	private static void printOutput(int numInputs, int outputNum) {
+		System.out.print("System.out.print(out" + outputNum + "(");
+		for (int j = 0; j < numInputs - 1; j++) {
+			System.out.print("in" + j + ",");
+		}
+		System.out.print("in" + numInputs + ")");
+		System.out.println(");");
+	}
+	
+	// Print an indent
+	private static void indent(int level) {
+		for (int i = 0; i < level; i++) {
+			System.out.print("    ");
+		}
 	}
 }
